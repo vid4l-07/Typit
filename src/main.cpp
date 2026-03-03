fe584a415c7906100840470b8d68217bc48f156f
Hugo Vidal <hugovima09@gmail.com>
2026-03-03 23:59:01 +0100

    ctrlc handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#include <csignal>
 #include <termios.h>
 #include "player.h"
 #include "game.h"
 #include "render.h"
+
+Game* game_point = nullptr;
+
+void ctrlc_handler(int signal) {
+	game_point -> end();
+	std::_Exit(0);
+}
 
 int main(){
     termios oldt;
@@ -12,6 +20,10 @@ int main(){
 	std::cout << "Modo: ";
 	std::cin >> mode;
 	Game game(player, render, mode);
+
+	game_point = &game;
+
+	std::signal(SIGINT, ctrlc_handler);
 
 	game.start();
     game.end();
